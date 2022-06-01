01a473d770f606c5de25bec95ee2ab57fec86fe1
Philip G Brodrick <philip.brodrick@jpl.nasa.gov>
2022-06-01 12:08:06 -0700

    Update main.py

--- main.py
+++ main.py
@@ -33,31 +33,33 @@ def main():
     timefunc(gdal_line_by_line_read, 'GDAL Line by Line Read', {args.input_file})
     timefunc(gdal_full_read, 'GDAL Full Read', {args.input_file})
     timefunc(envi_full_read, 'ENVI Full Read', {args.input_file})
-    #timefunc(rio_full_read, 'RasterIO Full Read', {args.input_file})
+    
+    
+    ''' TODO - benchmark COGs
+    timefunc(rio_full_read, 'RasterIO Full Read', {args.input_file})
 
-    #dat = envi_full_read_return(args.input_file)
-    #print('\n\nGeotiff Reads and Writes\n\n')
-    #timefunc(write_geotiff, 'Geotiff Write - Basic', (dat, os.path.join(args.scratch_dir, 'geotiff_basic.tif'), []))
-    #timefunc(gdal_line_by_line_read, 'GDAL Line by Line Read', {os.path.join(args.scratch_dir, 'geotiff_basic.tif')})
-    #timefunc(gdal_full_read, 'GDAL Full Read', {os.path.join(args.scratch_dir, 'geotiff_basic.tif')})
-    #print('\n')
-    #timefunc(write_geotiff, 'Geotiff Write - Tiled', (dat, os.path.join(args.scratch_dir, 'geotiff_t.tif'), ['TILED=YES']))
-    #timefunc(gdal_line_by_line_read, 'GDAL Line by Line Read', {os.path.join(args.scratch_dir, 'geotiff_t.tif')})
-    #timefunc(gdal_full_read, 'GDAL Full Read', {os.path.join(args.scratch_dir, 'geotiff_t.tif')})
-    #print('\n')
-    #timefunc(write_geotiff, 'Geotiff Write - Tiled, LZW', (dat, os.path.join(args.scratch_dir, 'geotiff_tc.tif'), ['TILED=YES', 'COMPRESS=LZW']))
-    #timefunc(gdal_line_by_line_read, 'GDAL Line by Line Read', {os.path.join(args.scratch_dir, 'geotiff_tc.tif')})
-    #timefunc(gdal_full_read, 'GDAL Full Read', {os.path.join(args.scratch_dir, 'geotiff_tc.tif')})
+    dat = envi_full_read_return(args.input_file)
+    print('\n\nGeotiff Reads and Writes\n\n')
+    timefunc(write_geotiff, 'Geotiff Write - Basic', (dat, os.path.join(args.scratch_dir, 'geotiff_basic.tif'), []))
+    timefunc(gdal_line_by_line_read, 'GDAL Line by Line Read', {os.path.join(args.scratch_dir, 'geotiff_basic.tif')})
+    timefunc(gdal_full_read, 'GDAL Full Read', {os.path.join(args.scratch_dir, 'geotiff_basic.tif')})
+    print('\n')
+    timefunc(write_geotiff, 'Geotiff Write - Tiled', (dat, os.path.join(args.scratch_dir, 'geotiff_t.tif'), ['TILED=YES']))
+    timefunc(gdal_line_by_line_read, 'GDAL Line by Line Read', {os.path.join(args.scratch_dir, 'geotiff_t.tif')})
+    timefunc(gdal_full_read, 'GDAL Full Read', {os.path.join(args.scratch_dir, 'geotiff_t.tif')})
+    print('\n')
+    timefunc(write_geotiff, 'Geotiff Write - Tiled, LZW', (dat, os.path.join(args.scratch_dir, 'geotiff_tc.tif'), ['TILED=YES', 'COMPRESS=LZW']))
+    timefunc(gdal_line_by_line_read, 'GDAL Line by Line Read', {os.path.join(args.scratch_dir, 'geotiff_tc.tif')})
+    timefunc(gdal_full_read, 'GDAL Full Read', {os.path.join(args.scratch_dir, 'geotiff_tc.tif')})
 
-    #print('\n\nCOG Reads and Writes\n\n')
-    #timefunc(write_cog, 'COG Write - Tiled, LZW', (dat, os.path.join(args.scratch_dir, 'geotiff_cog.tif'), ['TILED=YES', 'COMPRESS=LZW']))
-    #timefunc(gdal_line_by_line_read, 'GDAL Line by Line Read', {os.path.join(args.scratch_dir, 'geotiff_cog.tifcog')})
-    #timefunc(gdal_full_read, 'GDAL Full Read', {os.path.join(args.scratch_dir, 'geotiff_cog.tifcog')})
+    print('\n\nCOG Reads and Writes\n\n')
+    timefunc(write_cog, 'COG Write - Tiled, LZW', (dat, os.path.join(args.scratch_dir, 'geotiff_cog.tif'), ['TILED=YES', 'COMPRESS=LZW']))
+    timefunc(gdal_line_by_line_read, 'GDAL Line by Line Read', {os.path.join(args.scratch_dir, 'geotiff_cog.tifcog')})
+    timefunc(gdal_full_read, 'GDAL Full Read', {os.path.join(args.scratch_dir, 'geotiff_cog.tifcog')})
+    '''
 
     dat = envi_full_read_return(args.input_file)
     print('\n\nNETCDF Reads and Writes\n\n')
-    #timefunc(write_netcdf, 'NETCDF Write - Uncompressed', (dat, os.path.join(args.scratch_dir, 'netcdf.cf'), 0, None))
-    #timefunc(netcdf_full_read, 'NETCDF Full Read', {os.path.join(args.scratch_dir, 'netcdf.cf')})
 
     for complevel in [0,1,4,9]:
         fname = os.path.join(args.scratch_dir, f'netcdf_c{complevel}.cf')
